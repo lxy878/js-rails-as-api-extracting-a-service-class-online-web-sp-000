18d5c47984844e4a1d7f1bd0db4995dd92829dc7
Xiaoyi Liu <lxy878@gmail.com>
2020-08-24 03:00:25 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -9,8 +9,8 @@ class SightingsController < ApplicationController
   end
 
   def show
-      @sighting = Sighting.find(params[:id])
-      render json: @sighting.to_json(:include => {
+      sighting = Sighting.find(params[:id])
+      render json: sighting.to_json(:include => {
         :bird => {:only =>[:name, :species]},
         :location => {:only =>[:latitude, :longitude]}
       }, :except => [:updated_at])
