3e792e3e0fcbb8b9e5ad3bffa5c54253ccb54efa
Xiaoyi Liu <lxy878@gmail.com>
2020-08-24 03:08:48 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,7 +2,7 @@ class SightingsController < ApplicationController
 
   def index
     sightings = Sighting.all
-    render json: SightingSerializer.new(sighting).to_serialized_json
+    render json: SightingSerializer.new(sightings).to_serialized_json
   end
 
   def show
